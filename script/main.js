66db4c268db89158e6a585470a2c93d0cc86acb0
Kourosh Alasti <114209503+kourosh-alasti@users.noreply.github.com>
2024-04-08 17:31:10 -0700

    Update main.js
    added helper functions to disable prev / next button when first or last question

--- script/main.js
+++ script/main.js
@@ -35,7 +35,9 @@ function displayQuestion({ questions, questionIndex, quizDisplayContainer }) {
     .join("<br>");
 
   quizDisplayContainer.innerHTML = `
-        <div class="question">${currentQuestion.text}</div>
+        <div class="question">${questionIndex + 1} : ${
+    currentQuestion.text
+  }</div>
         <div class="options">${options}</div>
     `;
 }
@@ -63,10 +65,39 @@ function showResults({
   resultDisplayContainer.innerHTML = `Your Score is ${score} out of ${quizData.quiz.questions_count}`;
 }
 
+function changeBtnState({ btnRef, isDisabled }) {
+  btnRef.disabled = isDisabled;
+}
+
+function isFirstQuestion({ currentQuestionIndex, prevBtn, nextBtn }) {
+  const isFirst = currentQuestionIndex === 0;
+
+  if (isFirst) {
+    changeBtnState({ btnRef: prevBtn, isDisabled: true });
+  } else {
+    changeBtnState({ btnRef: prevBtn, isDisabled: false });
+  }
+
+  changeBtnState({ btnRef: nextBtn, isDisabled: false });
+}
+
+function isLastQuestion({ currentQuestionIndex, length, prevBtn, nextBtn }) {
+  const isLast = currentQuestionIndex === length - 1;
+
+  if (isLast) {
+    changeBtnState({ btnRef: nextBtn, isDisabled: true });
+  } else {
+    changeBtnState({ btnRef: nextBtn, isDisabled: false });
+  }
+
+  changeBtnState({ btnRef: prevBtn, isDisabled: false });
+}
+
 (async function () {
   const quizContainer = document.getElementById("quiz");
   const resultContainer = document.getElementById("results");
   const submitBtn = document.getElementById("submit");
+
   const prevBtn = document.getElementById("previous");
   const nextBtn = document.getElementById("next");
 
@@ -78,9 +109,30 @@ function showResults({
 
   let currentQuestionIndex = 0;
 
+  isFirstQuestion({
+    currentQuestionIndex: currentQuestionIndex,
+    prevBtn: prevBtn,
+    nextBtn: nextBtn,
+  });
+
   prevBtn.addEventListener("click", () => {
-    if (currentQuestionIndex > 0) {
+    changeBtnState({ btnRef: nextBtn, isDisabled: false });
+
+    if (currentQuestionIndex > 1) {
       currentQuestionIndex--;
+      displayQuestion({
+        questions: questions,
+        questionIndex: currentQuestionIndex,
+        quizDisplayContainer: quizContainer,
+      });
+    } else if (currentQuestionIndex === 1) {
+      currentQuestionIndex--;
+      isFirstQuestion({
+        currentQuestionIndex: currentQuestionIndex,
+        prevBtn: prevBtn,
+        nextBtn: nextBtn,
+      });
+
       displayQuestion({
         questions: questions,
         questionIndex: currentQuestionIndex,
@@ -90,8 +142,24 @@ function showResults({
   });
 
   nextBtn.addEventListener("click", () => {
-    if (currentQuestionIndex < quizLength - 1) {
+    changeBtnState({ btnRef: prevBtn, isDisabled: false });
+
+    if (currentQuestionIndex < quizLength - 2) {
       currentQuestionIndex++;
+      displayQuestion({
+        questions: questions,
+        questionIndex: currentQuestionIndex,
+        quizDisplayContainer: quizContainer,
+      });
+    } else if (currentQuestionIndex === quizLength - 2) {
+      currentQuestionIndex++;
+      isLastQuestion({
+        currentQuestionIndex: currentQuestionIndex,
+        length: quizLength,
+        prevBtn: prevBtn,
+        nextBtn: nextBtn,
+      });
+
       displayQuestion({
         questions: questions,
         questionIndex: currentQuestionIndex,
